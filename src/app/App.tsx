8b49de8ba685b13c3b339d76e2d2397f6a804476
Gabriel <criacao@novadimensaodigital.com.br>
2026-02-19 17:48:11 -0300

    feat: add onboarding page with Supabase integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { HeroSection } from "./components/sol/HeroSection";
 import { CardStackSection } from "./components/sol/CardStackSection";
 import { TestimonialsSection } from "./components/sol/TestimonialsSection";
@@ -5,13 +6,14 @@ import { ExpertSection } from "./components/sol/ExpertSection";
 import { PricingSection } from "./components/sol/PricingSection";
 import { FAQSection } from "./components/sol/FAQSection";
 import { ScrollBackgroundOrbs } from "./components/sol/ScrollBackgroundOrbs";
+import { OnboardingPage } from "./components/sol/OnboardingPage";
 
-export default function App() {
+function LandingPage() {
   return (
     <main className="relative">
       <HeroSection />
       <CardStackSection />
-      
+
       {/* Light Sections Group with Unified Parallax */}
       <div className="relative bg-[#FFFAFA] overflow-hidden">
         <ScrollBackgroundOrbs />
@@ -20,12 +22,21 @@ export default function App() {
       </div>
 
       <PricingSection />
-      
+
       <div className="bg-[#0D0D0D] flex justify-center items-center py-2 relative z-10">
         <div className="w-20 h-1.5 bg-[#CC3300] rounded-full opacity-80 shadow-[0_0_15px_rgba(204,51,0,0.4)]" />
       </div>
 
       <FAQSection />
     </main>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/obrigado" element={<OnboardingPage />} />
+    </Routes>
   );
 }
